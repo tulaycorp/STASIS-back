c90512e07d604c8ed07efb8c312c5751fef0a25a
Wendell <Angulo@gmail.com>
2025-06-15 00:45:15 +0800

    removed overrides and small cleanups made

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './styles/global-layout.css';
-import './styles/layout-overrides.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 
